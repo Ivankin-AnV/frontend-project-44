f616404a428c426a65eb290a53b6c9c37ac9a39d
Dodoko <toni.ivankin.04@mail.ru>
2023-12-17 17:12:26 +0500

    Изменил код в progression.js

--- src/logic-game/progression.js
+++ src/logic-game/progression.js
@@ -1,32 +1,57 @@
-import startGame from '../index.js';
-import { getRandomNumber } from '../randomNumber.js';
+import readlineSync from 'readline-sync';
 
-const progressionlength = 10;
-const description = 'What number is missing in the progression?';
-
-const getArithmeticProgression = () => {
-  let progressionNumber = getRandomNumber(0, 100);
-  const step = getRandomNumber(2, 100);
+const generateProgression = (start, step, length) => {
   const progression = [];
-
-  for (let i = 0; i < progressionlength; i += 1) {
-    progression.push(progressionNumber);
-    progressionNumber += step;
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + i * step);
   }
   return progression;
 };
 
-const getQuestionAndAnswer = () => {
-  const progression = getArithmeticProgression();
-  const randomIndex = getRandomNumber(0, progressionlength - 1);
-
-  const correctAnswer = String(progression[randomIndex]);
-  progression[randomIndex] = '..';
-  const question = progression;
-
-  return [question, correctAnswer];
+const hideNumber = (progression) => {
+  const hiddenIndex = Math.floor(Math.random() * progression.length);
+  const hiddenNumber = progression[hiddenIndex];
+  const question = progression.map((num, index) => (index === hiddenIndex ? '..' : num));
+  return { question, hiddenNumber };
 };
 
+const startGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+
+  console.log('What number is missing in the progression?');
+
+  let correctAnswers = 0;
+  let isGameOver = false;
+
+  while (!isGameOver) {
+    const start = Math.floor(Math.random() * 10) + 1;
+    const step = Math.floor(Math.random() * 10) + 1;
+    const length = Math.floor(Math.random() * 6) + 5;
+
+    const progression = generateProgression(start, step, length);
+    const { question, hiddenNumber } = hideNumber(progression);
+
+    console.log(`Question: ${question.join(' ')}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (Number(userAnswer) === hiddenNumber) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${hiddenNumber}'.`);
+      console.log(`Let's try again, ${name}!`);
+      isGameOver = true;
+    }
+
+    if (correctAnswers === 3) {
+      console.log(`Congratulations, ${name}!`);
+      isGameOver = true;
+    }
+    console.log();
+  }
+};
 export default () => {
-  startGame(description, getQuestionAndAnswer);
+  startGame();
 };
