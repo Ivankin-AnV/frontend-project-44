68365764204214ea81e275a8a36f398aaab8fa35
Dodoko <toni.ivankin.04@mail.ru>
2024-04-22 13:57:37 +0500

    refactoring

--- src/logic-game/even.js
+++ src/logic-game/even.js
@@ -1,18 +1,16 @@
-import runGame from '../index.js';
-import {
-  getRandomNumber,
-  isEvenNumber,
-} from '../randomNumber.js';
+import getRandomNum from '../randomNumber.js';
+import startGame from '../index.js';
+
+const isEven = (num) => num % 2 === 0;
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getQuestionAndAnswer = () => {
-  const question = getRandomNumber(0, 100);
-  const correctAnswer = isEvenNumber(question) ? 'yes' : 'no';
-
-  return [question, correctAnswer];
+  const question = getRandomNum();
+  const answer = isEven(question) ? 'yes' : 'no';
+  return [question, answer];
 };
 
-export default () => {
-  runGame(description, getQuestionAndAnswer);
-};
+const start = () => startGame(getQuestionAndAnswer, description);
+
+export default start;
--- src/logic-game/gcd.js
+++ src/logic-game/gcd.js
@@ -1,5 +1,5 @@
 import runGame from '../index.js';
-import { getRandomNumber } from '../randomNumber.js';
+import getRandomNumber from '../randomNumber.js';
 
 const description = 'Find the greatest common divisor of given numbers';
 
