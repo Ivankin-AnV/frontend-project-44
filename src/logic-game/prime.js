f410ab78576c4f6716cbc61ee2a1e05ddddc8e02
Dodoko <toni.ivankin.04@mail.ru>
2024-04-22 14:55:07 +0500

    refactoring

--- src/logic-game/prime.js
+++ src/logic-game/prime.js
@@ -1,24 +1,28 @@
 import startGame from '../index.js';
 import getRandomNumber from '../randomNumber.js';
 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
-
-const isPrimeNumber = (number) => {
-  for (let a = 2; a < number; a += 1) {
-    if (number % a === 0) {
+const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
       return false;
     }
   }
+
   return true;
 };
 
-const getQuestionAndAnswer = () => {
-  const question = getRandomNumber(2, 50);
-  const correctAnswer = isPrimeNumber(question) ? 'yes' : 'no';
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const generateRoundsData = () => {
+  const question = getRandomNumber();
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
 
 export default () => {
-  startGame(description, getQuestionAndAnswer);
+  startGame(gameDescription, generateRoundsData);
 };
